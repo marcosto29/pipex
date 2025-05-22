209a54304ff62e8d56d0f1fd63c24ba1bbd98ba8
Marcos <marcos29diciembre@gmail.com>
2025-05-22 14:41:20 +0200

    input stdin

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: matoledo <matoledo@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 16:55:36 by matoledo          #+#    #+#             */
-/*   Updated: 2025/05/22 12:52:18 by matoledo         ###   ########.fr       */
+/*   Updated: 2025/05/22 14:40:07 by matoledo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ static void	search_command(char *command, int pipe[])
 		close(pipe[1]);
 		dup2(save_stdout, 1);
 		perror("Error finding the command");
-		exit(EXIT_FAILURE);
+		exit(1);
 	}
 }
 
@@ -48,13 +48,13 @@ static char	*command_parse(char *command)
 	if (pipe(fd) == -1)
 	{
 		perror("pipe error");
-		exit(EXIT_FAILURE);
+		exit(1);
 	}
 	pid = fork();
 	if (pid < 0)
 	{
 		perror("fork error");
-		exit(EXIT_FAILURE);
+		exit(1);
 	}
 	if (pid == 0)
 		search_command(command, fd);
@@ -73,27 +73,27 @@ int	main(int argc, char *argv[])
 	char	**splitted_command;
 	char	*cmd;
 	char	*env_vec[] = { NULL };
-	char	*input_file;
-	int		fd;
+	int		fdi;
+	int		fdo;
 
 	argv++;
 	input_parse(argc, argv);
-	input_file = ft_strdup(*argv);
+	fdi = open(*argv, O_RDONLY);
+	dup2(fdi, 0);
 	argv++;
 	splitted_command = ft_split(*argv, ' ');
 	cmd = command_parse(*splitted_command);
 	if (!cmd || !*cmd)
 	{
 		perror("Error finding the command");
-		exit(EXIT_FAILURE);
+		exit(1);
 	}
-	splitted_command = add_string(splitted_command, input_file);
-	fd = open(last_string(argv), O_WRONLY | O_CREAT | O_TRUNC | O_APPEND, 0644);
-	dup2(fd, 1);
+	fdo = open(last_string(argv), O_WRONLY | O_CREAT | O_TRUNC | O_APPEND, 0644);
+	dup2(fdo, 1);
 	if (execve(cmd, splitted_command, env_vec) == -1)
 	{
 		perror("Error executing the command");
-		exit(EXIT_FAILURE);
+		exit(1);
 	}
 	return (0);
 }
